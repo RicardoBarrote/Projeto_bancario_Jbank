ec5d3be86dc43e06c729738921e560d505a88750
RicardoBarrote <ricardoambscfc@gmail.com>
2025-06-12 18:11:38 -0300

    Interface para JPA Reflection

---
+++ src/main/java/com/fbr/jbank/repository/dto/StatementView.java
@@ -0,0 +1,19 @@
+package com.fbr.jbank.repository.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public interface StatementView {
+
+    String getStatementId();
+
+    String getType();
+
+    BigDecimal getStatementValue();
+
+    String getWalletReceiver();
+
+    String getWalletSender();
+
+    LocalDateTime getStatementDateTime();
+}
